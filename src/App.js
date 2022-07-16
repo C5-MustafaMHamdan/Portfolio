4814dc51c88dbe6f07e87d5d037de61ff0a6c4a9
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-07-16 04:23:28 +0300

    fix-design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import "./App.css";
 import "./App.scss";
-import {
-  Button,
-  Card,
-  ListGroup,
-  ListGroupItem,
-  Container,
-  CardGroup,
-} from "react-bootstrap";
-import AboutMe from "./components/aboutMe";
-import Navbar from "./components/navbar";
-import Experience from "./components/experience";
+
 import React, { useRef } from "react";
 /*  import 'bootstrap/dist/css/bootstrap.min.css'; */
 
@@ -33,8 +23,9 @@ function App() {
       <div className="navbar">
         <button onClick={scrollOnAbout}> About</button>
         <button onClick={scrollOnExperience}> Projects</button>
-        <button onClick={scrollOnContact}> Contact</button>
         <button onClick={scrollOnSkills}> Skills</button>
+        <button onClick={scrollOnContact}> Contact</button>
+       
       </div>
 
       <div className="about">
@@ -47,12 +38,19 @@ function App() {
         </div>
 
         <div className="about-right">
-          <img
-            className="myImg"
-            src="https://media-exp2.licdn.com/dms/image/C4E03AQFqzpYxgLmVkg/profile-displayphoto-shrink_200_200/0/1656766220828?e=1661990400&v=beta&t=YcTNSOnBNtyf7A0eB8reH5IccDkKLxkDmjVvZS-uuxk"
-          />
+          
         </div>
       </div>
+
+
+
+
+
+
+
+
+
+
 
       <div ref={skills} class="all">
         <div class="lefter" id="MySQL">
@@ -90,170 +88,89 @@ function App() {
         </div>
       </div>
 
-      {/* 
-      <div ref={myExperience} className="my-experience">
-        <div className="header">
-          <h2>  My Projects </h2>
-        </div>
-        <div className="projects">
-          <div className="item">
-          <div className="img-div">
-            <img
-              className="cardImg"
-              variant="top"
-              src="https://res.cloudinary.com/dnin1rp5m/image/upload/v1656800895/discord_xcmquo.jpg"
-            /></div>
-            <div>
-              <div className="title">
-                {" "}
-                <a href="https://github.com/C5-MustafaMHamdan/Project-5">
-                  Discord Clone
-                </a>{" "}
-              </div>
-              <div className="desc">
-                It is a discord clone with the same name as the app which it has
-                most of the features used
-              </div>
-            </div>
-           
-          </div>
-          <div className="item" >
-            <div className="img-div" >
-            <img
-              
-              className="cardImg"
-              src="https://res.cloudinary.com/dnin1rp5m/image/upload/v1656800802/docr_ggl4lw.jpg"
-            /> </div>
-            <div className="item">
-              <Card.Title className="title"><a href="https://github.com/C5-MustafaMHamdan/MERAKI_Academy_Project_4">Appoint.Me </a></Card.Title>
-              <div className="desc">
-                An application to organize appointments between doctors and
-                patients
-              </div>
-            </div>
-            <Card.Footer>
-       
-            </Card.Footer>
-          </div>
-        </div>
-      </div>     
-  */}
+      
 
-      {/*             test1                          */}
 
-      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-<div id="container">	
-	
-	<div class="product-details">
-		
-	<h1>Discord Clone</h1>
-	<span class="hint-star star">
-		<i class="fa fa-star" aria-hidden="true"></i>
-		<i class="fa fa-star" aria-hidden="true"></i>
-		<i class="fa fa-star" aria-hidden="true"></i>
-		<i class="fa fa-star" aria-hidden="true"></i>
-		<i class="fa fa-star-o" aria-hidden="true"></i>
-	</span>
-		
-			<p class="information">It is a discord clone with the same name as the app which it has
-                most of the features used</p>
-
-		
-		
-<div class="control">
-	
-	<button class="btn">
-	 <span class="price">$250</span>
-   <span class="shopping-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></span>
-   <span class="buy">Get now</span>
- </button>
-	
-</div>
-			
-</div>
-	
-<div class="product-image">
-	
-	<img src="https://res.cloudinary.com/dnin1rp5m/image/upload/v1656800895/discord_xcmquo.jpg" alt=""/>
-	
-
-<div class="info">
-	<h2> Description</h2>
-	<ul>
-		<li><strong>Height : </strong>5 Ft </li>
-		<li><strong>Shade : </strong>Olive green</li>
-		<li><strong>Decoration: </strong>balls and bells</li>
-		<li><strong>Material: </strong>Eco-Friendly</li>
-		
-	</ul>
-</div>
-</div>
-
-</div> */}
-
-      {/* test 3 */}
-
-      <div ref={myExperience} class="card-container">
-        <h2> My Projects </h2>
-
-        <div class="cardy" id="imgy1">
-          <a href="https://github.com/C5-MustafaMHamdan/Project-5">
+     
+      <div   ref={myExperience}     class="wrapper2">
+        <div  class="wrapper2-title"><h1 >My Projects</h1></div>
+        <div   class="projects" >
+        <div  class="cardz"  >
+            <a href="https://github.com/C5-MustafaMHamdan/Project-5">
             Discord Clone
           </a>
-          <p>
-            {" "}
-            It is a discord clone with the same name as the app which it has
-            most of the features used
-          </p>
+            <img src="https://res.cloudinary.com/dnin1rp5m/image/upload/v1656800895/discord_xcmquo.jpg" 
+            />
+            
+            <div class="descriptions">
+                <h1>Discord Clone</h1>
+                <p>
+                It is a discord clone with the same name as the app which it has
+            most of the features used               </p>
+            <a href="https://github.com/C5-MustafaMHamdan/Project-5">
+            Discord Clone
+          </a>
+            </div>
+
         </div>
 
-        <div class="cardy" id="imgy2">
-          <a href="https://github.com/C5-MustafaMHamdan/MERAKI_Academy_Project_4">
+        <div class="cardz">
+            <img src="https://res.cloudinary.com/dnin1rp5m/image/upload/v1656800802/docr_ggl4lw.jpg"/>
+            <div class="descriptions">
+                <h1>Appoint.Me</h1>
+                <p>
+                An application to organize appointments between doctors and patients             </p>
+            <a href="https://github.com/C5-MustafaMHamdan/MERAKI_Academy_Project_4">
             Appoint.Me{" "}
           </a>
-          <p>
-            {" "}
-            An application to organize appointments between doctors and patients
-          </p>
+            </div>
         </div>
-      </div>
+        </div>
+    </div>
 
-      <div ref={myContact} className="contact-me">
-        <div className="findMe">
-          <h2>You Can Find Me There </h2>
-        </div>
-        <div className="contactMe">
-          <div className="linkdIn">
-            <a href="https://www.linkedin.com/in/mustafahamda/">
-              <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png" />
-            </a>
-          </div>
-          <div className="gitHub">
-            {" "}
-            <a href="https://github.com/MustafaMHamdan">
+
+
+
+
+
+
+
+ 
+
+
+      <div ref={myContact} class="infocardContainer">
+  <div id="main">
+    <img src="https://media-exp2.licdn.com/dms/image/C4E03AQFqzpYxgLmVkg/profile-displayphoto-shrink_200_200/0/1656766220828?e=1661990400&v=beta&t=YcTNSOnBNtyf7A0eB8reH5IccDkKLxkDmjVvZS-uuxk"></img>
+  </div>
+  <div id="textbois">
+    <h2>Mustafa Hamdan</h2>
+    <h4>Full-stack Developer</h4>
+    <a href="hamdan.mustafamof@gmail.com">hamdan.mustafamof@gmail.com</a>
+    <div id="hotlinks">
+    <a href="https://github.com/MustafaMHamdan">
               {" "}
               <img src="https://iconape.com/wp-content/png_logo_vector/github-circle-coreui-icons-v1-0-0.png" />
             </a>{" "}
-          </div>{" "}
-        </div>
-      </div>
+            <a href="https://www.linkedin.com/in/mustafahamda/">
+              <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png" />
+            </a>
+    
+    </div>
+  </div>
+</div> 
 
-      <div className="container">
-        <div className="card">
-          <div className="front">
-            <h1> You Can contact here</h1>
-          </div>
-          <div className="back">
-            <h1>
-              Mustafa Hamdan<span>Full-stack Developer </span>
-            </h1>
 
-            <h1> +962790863868 </h1>
 
-            <h1> hamdan.mustafamof@gmail.com</h1>
-          </div>
-        </div>
-      </div>
+    
+
+
+
+
+
+
+
+
+    
     </div>
   );
 }
--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -1,15 +1,1 @@
-import "./style.css";
-import React, { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-const AboutMe = () => {
-  return (
-    <div className="about-me">
-      <h1> Hi,</h1> <br></br> <h1> I am Mustafa Hamdan Web Developer </h1>
-      <br></br>
-      <h2>Full-Stack Developer</h2>
-    </div>
-  );
-};
-
-export default AboutMe;
+ 
